8e5d15325796d371aa020aff10a551e7e97d1407
Luca Gambetta <l.gambetta@bluenine.it>
2020-07-26 12:31:57 +0200

    Added the wishlist item resource and make use of it

--- app/Http/Resources/WishlistResource.php
+++ app/Http/Resources/WishlistResource.php
@@ -14,6 +14,14 @@ class WishlistResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'name' => $this->name,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'items' => WishlistItemResource::collection($this->items)
+
+        ];
     }
 }
