190171cf126aad42404ce59f091bc6d12aeb12cf
Luca Gambetta <l.gambetta@nexsoft.it>
2020-07-27 09:18:22 +0200

    Implemented the wishlist seeder

--- database/seeds/WishlistSeeder.php
+++ database/seeds/WishlistSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\User;
+use App\Wishlist;
+use App\WishlistItem;
 use Illuminate\Database\Seeder;
 
 class WishlistSeeder extends Seeder
@@ -11,6 +14,26 @@ class WishlistSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Make some users and seed the DB
+        $user1 = factory(User::class);
+        $wishlist = factory(Wishlist::class)->create([
+            'user_id' => $user1
+        ]);
+        factory(WishlistItem::class, 5)->create([
+            'wishlist_id' => $wishlist
+        ]);
+
+        $user2 = factory(User::class);
+        factory(Wishlist::class)->create([
+            'user_id' => $user2
+        ]);
+
+        $user3 = factory(User::class);
+        $wishlist2 = factory(Wishlist::class)->create([
+            'user_id' => $user3
+        ]);
+        factory(WishlistItem::class, 3)->create([
+            'wishlist_id' => $wishlist2
+        ]);
     }
 }
