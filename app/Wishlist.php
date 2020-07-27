6f33a0a1828eeae863f9eb9dacfafae08e508dc2
Luca Gambetta <l.gambetta@nexsoft.it>
2020-07-27 09:17:58 +0200

    Addded a method to get the grand total of wishlists

--- app/Wishlist.php
+++ app/Wishlist.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Models a wishlist
@@ -21,5 +22,21 @@ class Wishlist extends Model
     public function items()
     {
         return $this->hasMany(WishlistItem::class, 'wishlist_id');
+    }
+
+    /**
+     * Return a total by wishlist 
+     *
+     * @return array
+     */
+    public static function wishlistReport()
+    {
+        return DB::table('wishlists')->
+        join('users', 'wishlists.user_id', '=', 'users.id')->
+        leftJoin('wishlists_items', 'wishlists.id', '=', 'wishlists_items.wishlist_id')->
+        select('users.name as user', 'wishlists.name as wishlist', DB::raw('count(`wishlists_items`.`id`) as total'))->
+        groupBy('wishlists_items.wishlist_id', 'users.name', 'wishlists.name')->
+        orderBy('users.name')->
+        get();
     }
 }
